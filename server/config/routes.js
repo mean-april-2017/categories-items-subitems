611cf8f489769d817bb5559844ebbe583cf4742e
David Wolgemuth <davidjwolgemuth@gmail.com>
2017-04-24 14:03:31 -0500

    routes for categories

--- server/config/routes.js
+++ server/config/routes.js
@@ -2,11 +2,14 @@
     Route Request to Appropriate Contoller
 */
 console.log("/server/config/routes.js");
+var categories = require("../controllers/categories");  // Require Categories Controller
 var items = require("../controllers/items");  // Require Items Controller
-var subItems = require("../controllers/sub-items");  // Require Items Controller
+var subItems = require("../controllers/sub-items");  // Require SubItems Controller
 
 module.exports = function (app)
 {
+    app.get("/api/categories", categories.index);
+    app.post("/api/categories", categories.create);
     app.get("/api/items", items.index);
     app.post("/api/items", items.create);
     app.post("/api/items/:itemId/subitems", subItems.create);
---
+++ server/controllers/categories.js
@@ -0,0 +1,14 @@
+/*
+    Handle Incoming Requests for Categories
+*/
+console.log("/server/controllers/categories.js");
+
+module.exports.index = function (request, response)
+{
+
+}
+
+module.exports.create = function (request, response)
+{
+  
+}
